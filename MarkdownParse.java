37417809548a9b872c6e09fff213984b573ecc67
c5k1m <christianshkim001@gmail.com>
2022-02-03 10:52:12 -0800

    Made Brief Changes

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) throws IOException {
         ArrayList<String> toReturn = new ArrayList<>();
@@ -16,31 +17,17 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
 
-<<<<<<< HEAD
-            if (nextOpenBracket == -1) {
-=======
             //checks for infinite looping
->>>>>>> 2ddf26f76403e0baa1789d5d0956e6c735c8d0d9
             if (nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) {
                 break;
             }
 
-<<<<<<< HEAD
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-
-=======
             //checks for brackets and parentheses with stuff between them, and empty links
             if (nextCloseBracket + 1 == openParen && openParen + 1 != closeParen) {
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
             
->>>>>>> 2ddf26f76403e0baa1789d5d0956e6c735c8d0d9
             currentIndex = closeParen + 1;
-
-            //System.out.println(currentIndex + " " + markdown.charAt(currentIndex-1));
         }
         return toReturn;
     }
